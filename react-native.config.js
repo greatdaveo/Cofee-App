3feb28edd3ea6fa417067466bc9f9514f4994c63
David Olowomeye <olowomeyedavid242@gmail.com>
2024-05-27 15:19:45 +0100

    Structure the code folder and added all necessary assets files

---
+++ react-native.config.js
@@ -0,0 +1,9 @@
+//This will help to append the asset to android and ios (run "npx react-native-asset" after)
+
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./src/assets'],
+};
