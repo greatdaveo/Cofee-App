56e6e5be2e9e9776203377f929806bb627b6e3a9
David Olowomeye <olowomeyedavid242@gmail.com>
2024-06-13 07:21:22 +0100

    Corrected the SafeAreaView Structure

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import {Platform, StatusBar, StyleSheet, Text, View} from 'react-native';
 import DetailScreen from './src/screens/DetailScreen';
 import PaymentScreen from './src/screens/PaymentScreen';
 import TabNavigator from './src/navigators/TabNavigator';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 
 // For Navigation
 import {NavigationContainer} from '@react-navigation/native';
@@ -14,31 +14,44 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.container}>
+        <StatusBar
+          barStyle="dark-content"
+          backgroundColor="transparent"
+          translucent={true}
+        />
 
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown: false}}>
-          <Stack.Screen
-            name="Tab"
-            component={TabNavigator}
-            options={{animation: 'slide_from_bottom'}}
-          />
-          <Stack.Screen
-            name="Details"
-            component={DetailScreen}
-            options={{animation: 'slide_from_bottom'}}
-          />
-          <Stack.Screen
-            name="Payment"
-            component={PaymentScreen}
-            options={{animation: 'slide_from_bottom'}}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen
+              name="Tab"
+              component={TabNavigator}
+              options={{animation: 'slide_from_bottom'}}
+            />
+            <Stack.Screen
+              name="Details"
+              component={DetailScreen}
+              options={{animation: 'slide_from_bottom'}}
+            />
+            <Stack.Screen
+              name="Payment"
+              component={PaymentScreen}
+              options={{animation: 'slide_from_bottom'}}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    marginBottom: -35,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+});
 
 export default App;
--- src/navigators/TabNavigator.tsx
+++ src/navigators/TabNavigator.tsx
@@ -1,86 +1,122 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import HomeScreen from '../screens/HomeScreen'
-import CartScreen from '../screens/CartScreen'
-import FavoriteScreen from '../screens/FavoriteScreen'
-import OrderHistoryScreen from '../screens/OrderHistoryScreen'
-import CustomIcon from '../components/CustomIcon'
-import { COLORS } from '../theme/theme'
-import { BlurView } from '@react-native-community/blur'
-
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import HomeScreen from '../screens/HomeScreen';
+import CartScreen from '../screens/CartScreen';
+import FavoriteScreen from '../screens/FavoriteScreen';
+import OrderHistoryScreen from '../screens/OrderHistoryScreen';
+import CustomIcon from '../components/CustomIcon';
+import {COLORS} from '../theme/theme';
+import {BlurView} from '@react-native-community/blur';
 
 // For Bottom Tabs
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-const Tab = createBottomTabNavigator()
-
+const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <Tab.Navigator screenOptions={{
-                headerShown: false,
-                tabBarHideOnKeyboard: true,
-                tabBarShowLabel: false,
-                tabBarStyle: styles.tabBarStyle,
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarHideOnKeyboard: true,
+        tabBarShowLabel: false,
+        tabBarStyle: styles.tabBarStyle,
+        tabBarBackground: () => (
+          <BlurView
+            overlayColor=""
+            blurAmount={15}
+            style={styles.BlurViewStyles}
+          />
+        ),
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({focused, color, size}) => (
+            <CustomIcon
+              name="home"
+              size={25}
+              color={
+                focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex
+              }
+            />
+          ),
+        }}
+      />
 
+      <Tab.Screen
+        name="Cart"
+        component={CartScreen}
+        options={{
+          tabBarIcon: ({focused, color, size}) => (
+            <CustomIcon
+              name="cart"
+              size={25}
+              color={
+                focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex
+              }
+            />
+          ),
+        }}
+      />
 
-                tabBarBackground: (
-                    () => (
-                        <BlurView overlayColor='' blurAmount={15} style={styles.BlurViewStyles} />
-                    ))
-            }}>
-                <Tab.Screen name='Home' component={HomeScreen} options={{
-                    tabBarIcon: ({ focused, color, size }) => (
-                        <CustomIcon name='home' size={25} color={focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex} />
-                    )
-                }} />
+      <Tab.Screen
+        name="Favorite"
+        component={FavoriteScreen}
+        options={{
+          tabBarIcon: ({focused, color, size}) => (
+            <CustomIcon
+              name="like"
+              size={25}
+              color={
+                focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex
+              }
+            />
+          ),
+        }}
+      />
 
-                <Tab.Screen name='Cart' component={CartScreen} options={{
-                    tabBarIcon: ({ focused, color, size }) => (
-                        <CustomIcon name='cart' size={25} color={focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex} />
-                    )
-                }} />
-
-                <Tab.Screen name='Favorite' component={FavoriteScreen}
-                    options={{
-                        tabBarIcon: ({ focused, color, size }) => (
-                            <CustomIcon name='like' size={25} color={focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex} />
-                        )
-                    }} />
-
-                <Tab.Screen name='History' component={OrderHistoryScreen}
-                    options={{
-                        tabBarIcon: ({ focused, color, size }) => (
-                            <CustomIcon name='bell' size={25} color={focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex} />
-                        )
-                    }} />
-            </Tab.Navigator>
-        </SafeAreaView>
-    )
-}
-
+      <Tab.Screen
+        name="History"
+        component={OrderHistoryScreen}
+        options={{
+          tabBarIcon: ({focused, color, size}) => (
+            <CustomIcon
+              name="bell"
+              size={25}
+              color={
+                focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex
+              }
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 const styles = StyleSheet.create({
-    tabBarStyle: {
-        height: 80,
-        // paddingBottom: 0,
-        marginBottom: -35,
-        position: "absolute",
-        backgroundColor: COLORS.primaryBlackRGBA,
-        borderTopWidth: 0,
-        elevation: 0,
-        borderTopColor: "transparent"
-    },
+  tabBarStyle: {
+    // flex: 1,
+    height: 80,
+    paddingBottom: 0,
+    marginBottom: 0,
+    position: 'absolute',
+    backgroundColor: COLORS.primaryBlackRGBA,
+    borderTopWidth: 0,
+    elevation: 0,
+    borderTopColor: 'transparent',
+  },
 
-    BlurViewStyles: {
-        position: "absolute",
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0
-    }
-})
+  BlurViewStyles: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+});
 
-export default TabNavigator
+export default TabNavigator;
